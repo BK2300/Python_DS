b5ec577ca7823512e8b362bab9511d5ebaebc304
BK2300 <BK2300@yahoo.com>
2025-10-06 09:58:11 +0200

    waste of life

--- excercises directionary/Excercise sheet 4 (loops)/4.py
+++ excercises directionary/Excercise sheet 4 (loops)/4.py
@@ -1,10 +1,15 @@
 # Write a program to display the pattern like a diamond.  Example:
 
+triangle_line = ""
+direction = 1
+number_wildcards = 12
 
+for i in range(1, number_wildcards * 2):
+    print(triangle_line + "*")
+    triangle_line += "*"
 
+    if len(triangle_line) == number_wildcards: #
+        direction = -1
 
-
-
-
-
-
+    if direction == -1:
+        triangle_line = triangle_line[:-2]
--- lectures follow along/7. Mutable Data Types.py
+++ lectures follow along/7. Mutable Data Types.py
@@ -1,16 +1,16 @@
 
+fruits = ("apple", "orange", "mango", "grapes")
 
+print(fruits[0])
 
+print(len(fruits))
 
+result = ""
+for fruit in fruits:
+    print(result)
 
-
-
-
-
-
-
-
-
+for i range(len(fruits)):
+    print("fruit in number", i, "is", fruits[i])
 
 
 
