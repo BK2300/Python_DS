75cce9ef3e52186877c55f5731dd39c55a305aaf
BK2300 <BK2300@yahoo.com>
2025-10-27 09:44:24 +0100

    Cleaning up some messy notations

---
+++ lectures follow along/10. functions.py
@@ -0,0 +1,20 @@
+def is_even (number_inputed):
+    if number_inputed % 2 == 0:
+        return True
+    else:
+        return False
+
+    #Call the function like this
+    is_even(11) #return False
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lectures follow along/12 modules & file IO.py
@@ -0,0 +1,29 @@
+
+
+
+# File IO
+"""
+- Open() return a file object
+- most commonly used with two arguments:
+open(filename, mode)
+
+Tror det betyder du har gemt en file eller dokument, som en variable. Så den er lettere at få adgang til
+"""
+
+
+# Absolute vs relative path
+# Absolute path = er den fulde linje "C\Bacon\Fizz\spam.txt" (eksempel fra leksionen)
+# Relative path = den kompakte linge "...\Fizz\spam.txt"
+
+
+
+
+
+
+
+
+
+
+
+
+
